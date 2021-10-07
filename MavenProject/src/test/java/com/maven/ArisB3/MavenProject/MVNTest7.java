9fd4b8e38c4a5023dde5867a00729a41a028fe66
00005232 <00005232@AGI-LT-847.arisglobal.com>
2021-10-07 12:57:47 +0530

    TEST

--- MavenProject/src/test/java/com/maven/ArisB3/MavenProject/MVNTest7.java
+++ MavenProject/src/test/java/com/maven/ArisB3/MavenProject/MVNTest7.java
@@ -8,17 +8,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class MVNTest6 {
+public class MVNTest7 {
 
 	public WebDriver driver;
 	
 	@Test(groups = "{R1}")
- 	@Parameters({"fbURL"})
-	public void OpenFB(String fbURL)
+ 		public void OpenAmazon()
 	{
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\00005232\\OneDrive - Arisglobal software pvt Ltd\\Softwares for installation\\chromedriver.exe");
     driver=new ChromeDriver();
-	driver.get(fbURL);
+	driver.get("http://www.amazon.in");
 	System.out.println(driver.getTitle());
 	driver.close();
 	}
