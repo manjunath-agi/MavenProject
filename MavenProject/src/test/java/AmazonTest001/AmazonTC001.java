5c3d3e6e404c718180f67ef7a491a7d0d4050fec
00005232 <00005232@AGI-LT-847.arisglobal.com>
2021-10-12 13:03:14 +0530

    ExtentReport

--- MavenProject/src/test/java/AmazonTest001/AmazonTC001.java
+++ MavenProject/src/test/java/AmazonTest001/AmazonTC001.java
@@ -7,23 +7,16 @@ import org.testng.annotations.Test;
 
 public class AmazonTC001 extends base {
 	
-	public WebDriver driver;
+	//public WebDriver driver;
 	
 	@Test
-	public void TestCase001()
+	public void TestCase001() throws Exception
 	{
-	base baseclassobj = new base();
-	try {
-		baseclassobj.BrowserCall();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
-	driver.get(prop.getProperty("URL"));
-	AmazonPOM APOM = new AmazonPOM(driver);
-	APOM.SearchFieldbox().sendKeys("iphone 13 pro");
-	APOM.SearchButton().click();
+		BrowserCall();
+		driver.get(prop.getProperty("URL"));
+		AmazonPOM APOM = new AmazonPOM(driver);
+		APOM.SearchFieldbox().sendKeys("iphone 13 pro");
+		APOM.SearchButton().click();
 	
 	
 	
